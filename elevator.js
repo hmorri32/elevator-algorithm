0733fabe91c182ac0e20249b0d4b58cb54258a8f
Hugh Morrison <hmorri32@gmail.com>
2017-05-09 17:23:21 -0600

    level2

--- elevator.js
+++ elevator.js
@@ -1,7 +1,64 @@
 export default class Elevator {
-  constructor() {
+  constructor(){
+  	this.riders 			= [];
+  	this.requests			= [];
+  	this.currentFloor = 0;
+  	this.totalFloors  = 0; 
+  	this.stops 		    = [];
+  	this.motionStatus = 'idle';
+  	this.direction 	 	= '';
   }
 
-  get currentFloor() {
+  goToFloor(person){
+  	this.requests.push(person)
+
+  	const { name, currentFloor, dropOffFloor } = person
+
+  	this.motionStatus = 'moving';
+  	this.currentFloor = currentFloor
+  	this.direction 		= this.elevatorDirection(currentFloor, dropOffFloor)
+  	this.stops.push(this.currentFloor)
+  	this.riders.push(name)
+  	this.getStops()
+  	this.motionStatus = 'idle'
+  	this.riderExit()
+  }
+
+  elevatorDirection(currentFloor, dropOffFloor) {
+  	return currentFloor > dropOffFloor ? 'up' : 'down';
+  }
+
+  riderExit() {
+  	return this.riders.shift()
+  }
+
+  getTotalFloors(request) {
+  	const { currentFloor, dropOffFloor } = request
+
+  	this.totalFloors = Math.abs(this.currentFloor - currentFloor) + Math.abs(currentFloor - dropOffFloor)
+
+  	this.currentFloor = dropOffFloor
+  }
+
+  getStops() {
+  	const { currentFloor, dropOffFloor } = this.requests
+
+  	return this.requests.reduce((arr, request) => {
+  		arr = []
+
+  		this.getTotalFloors(request)
+
+  		arr.push(request.currentFloor, request.dropOffFloor)
+  		return arr
+  	}, [])
+  }
+
+  reset(){
+  	this.constructor()
   }
 }
+
+
+
+
+
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -4,6 +4,7 @@ require('babel-core/register')({
 
 const assert   = require('chai').assert;
 const Elevator = require('../elevator').default;
+const Person   = require('../person').default;
 
 describe('Elevator', function() {
   let elevator = new Elevator();
@@ -13,21 +14,97 @@ describe('Elevator', function() {
   });
 
   it('should bring a rider to a floor above their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
+    let person = new Person('Brittany', 2, 5)
 
-    elevator.goToFloor(mockUser);
+    elevator.goToFloor(person);
 
     assert.equal(elevator.currentFloor, 5);
     assert.equal(elevator.motionStatus, 'idle');
-    assert.equal(elevator.getStops(), [2, 5]);
+    assert.deepEqual(elevator.getStops(), [2, 5]);
+
+    elevator.getTotalFloors(person)
+    assert.equal(elevator.totalFloors, 6)
   });
 
   it('should bring a rider to a floor below their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
-    elevator.goToFloor(mockUser);
+    let person = new Person('Brittany', 8, 3)
+
+    elevator.goToFloor(person);
 
     assert.equal(elevator.currentFloor, 3);
     assert.equal(elevator.motionStatus, 'idle');
-    assert.equal(elevator.getStops(), [8, 3]);
+    assert.deepEqual(elevator.getStops(), [8, 3]);
   });
+
+  it('should calculate amount of floors traversed going up', () => {
+    let person = new Person('Brittany', 2, 5)
+
+    elevator.goToFloor(person)
+
+    assert.equal(elevator.currentFloor, 5);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [2, 5]);
+
+    elevator.getTotalFloors(person)
+
+    assert.equal(elevator.totalFloors, 6)
+  })
+
+  it('should calculate amount of floors traversed going down', () => {
+    let person = new Person('Brittany', 8, 3)
+
+    elevator.goToFloor(person)
+
+    assert.equal(elevator.currentFloor, 3);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+
+    elevator.getTotalFloors(person)
+
+    assert.equal(elevator.totalFloors, 10)
+  })
+
+  it('should reset', () => {
+    let person = new Person('suhDude', 5,  2);
+
+    elevator.goToFloor(person);
+
+    assert.equal(elevator.currentFloor, 2);
+    assert.deepEqual(elevator.getStops(), [5, 2]);
+    assert.equal(elevator.totalFloors, 6);
+
+    elevator.reset();
+
+    assert.equal(elevator.currentFloor, 0);
+    assert.deepEqual(elevator.getStops(), []);
+    assert.equal(elevator.totalFloors, 0);
+    assert.deepEqual(elevator.riders, []);
+    assert.deepEqual(elevator.requests, []);
+  });
+
+
 });
+
+describe('elveator unit tests', () => {
+  it('testing reset', () => {
+
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
